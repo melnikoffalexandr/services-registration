29738dac0b356b3b46b4ea3aac17a75b2e59ffa8
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-17 20:58:52 +0500

    test 2

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../../pages/homePage';
-// import LayoutPage from '../../pages/layoutPage';
+import LayoutPage from '../../pages/layoutPage';
 import CreateNewEntry from '../../pages/createNewEntry';
 
 import styles from './app.module.scss';
@@ -10,8 +10,8 @@ import styles from './app.module.scss';
 export const App = () => (
     <div className={styles.root}>
         <Routes>
-            <Route path="/" element={<HomePage />}>
-                {/* <Route index element={} />*/}
+            <Route path="/" element={<LayoutPage />}>
+                <Route index element={<HomePage />} />
                 <Route path="/create-new-entry" element={<CreateNewEntry />} />
             </Route>
         </Routes>
