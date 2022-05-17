d766cc77796c64ecc84e813fdff7ac75245ff48d
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-17 20:40:37 +0500

    add homepage

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -10,13 +10,12 @@ import styles from './app.module.scss';
 export const App = () => (
     <div className={styles.root}>
         <Routes>
-            <Route path="/" element={<LayoutPage />}>
-                <Route index element={<HomePage />} />
-                <Route path="create-new-entry" element={<CreateNewEntry />} />
+            <Route path="/" element={<LayoutPage/>}>
+                <Route index element={<HomePage/>}/>
+                <Route path="/create-new-entry" element={<CreateNewEntry/>}/>
             </Route>
         </Routes>
     </div>
-
 );
 
 export default App;
