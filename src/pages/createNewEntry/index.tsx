25ae9f2de5b473197816d6864df68154fe411a13
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-18 13:44:58 +0500

    refactoring calendar and time picker

--- src/pages/createNewEntry/index.tsx
+++ src/pages/createNewEntry/index.tsx
@@ -5,6 +5,7 @@ import { format } from 'date-fns';
 import stylesPicker from 'react-day-picker/dist/style.module.css';
 import cls from 'classnames';
 import { Link } from 'react-router-dom';
+import { createPortal } from 'react-dom';
 
 import { ReactComponent as CalendarImg } from '../../assets/img/calendar.svg';
 import { ReactComponent as TimeImg } from '../../assets/img/time.svg';
@@ -16,15 +17,15 @@ import styles from './createNewEntry.module.scss';
 export const CreateNewEntry = () => {
     const calendarRef = useRef<HTMLDivElement>(null);
     const timeRef = useRef<HTMLDivElement>(null);
-    const [calendarIsOpen, setCalendarIsOpen] = useState(false);
-    const [timeIsOpen, setTimeIsOpen] = useState(false);
+
+    const [calendarPopup, setCalendarPopup] = useState({ isOpen: false, top: 0, left: 0 });
+    const [timePopup, setTimePopup] = useState({ isOpen: false, width: 0, top: 0, left: 0 });
     const [selectedDate, setSelectedDate] = useState<Date>();
     const [dateValue, setDateValue] = useState<string>('');
     const [timeValue, setTimeValue] = useState<string>('');
 
-
-    useOnClickOutside(calendarIsOpen, calendarRef, () => setCalendarIsOpen(false));
-    useOnClickOutside(timeIsOpen, timeRef, () => setTimeIsOpen(false));
+    useOnClickOutside(calendarPopup.isOpen, calendarRef, () => setCalendarPopup({ ...calendarPopup, isOpen: false }));
+    useOnClickOutside(timePopup.isOpen, timeRef, () => setTimePopup({ ...timePopup, isOpen: false }));
 
     const classNames: ClassNames = {
         ...stylesPicker,
@@ -36,12 +37,6 @@ export const CreateNewEntry = () => {
         nav_button: cls(stylesPicker.nav_button, styles.navButton)
     };
 
-    const handleDaySelect = (date: Date) => {
-        setSelectedDate(date);
-        setDateValue(format(date, 'dd.MM.yyyy'));
-        setCalendarIsOpen(!calendarIsOpen);
-    };
-
     const timeArr = ['8:00', '8:30', '9:00','9:30','10:00','10:30','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30', '19:00','19:30','20:00'];
 
     useEffect(() => {
@@ -49,7 +44,7 @@ export const CreateNewEntry = () => {
         if (el) {
             el.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
-    }, [timeIsOpen]);
+    }, [timePopup.isOpen]);
 
     return (
         <div className={styles.root}>
@@ -64,7 +59,11 @@ export const CreateNewEntry = () => {
                 <div className={styles.dateWrapper}>
                     <div
                         className={styles.inputWrapper}
-                        onClick={() => setCalendarIsOpen(!calendarIsOpen)}
+                        onClick={(event) => {
+                            const el = event.currentTarget;
+                            const { top, left } = el.getBoundingClientRect();
+                            setCalendarPopup({ isOpen: true, top: top, left: left });
+                        }}
                     >
                         <div className={styles.imageWrapper}>
                             <CalendarImg />
@@ -76,27 +75,14 @@ export const CreateNewEntry = () => {
                         <span className={styles.placeholder}>
                             {!dateValue && <span>Дата</span>}
                         </span>
-                        {calendarIsOpen && (
-                            <div ref={calendarRef}>
-                                <DayPicker
-                                    classNames={classNames}
-                                    showOutsideDays
-                                    defaultMonth={selectedDate}
-                                    locale={ru}
-                                    mode="single"
-                                    selected={selectedDate}
-                                    onDayClick={(date) => {
-                                        if (date) {
-                                            handleDaySelect(date);
-                                        }
-                                    }}
-                                />
-                            </div>
-                        )}
                     </div>
                     <div
                         className={styles.inputWrapper}
-                        onClick={() => setTimeIsOpen(!timeIsOpen)}
+                        onClick={(event) => {
+                            const el = event.currentTarget;
+                            const { top, left, width } = el.getBoundingClientRect();
+                            setTimePopup({ isOpen: true, width: width, top: window.pageYOffset + top, left });
+                        }}
                     >
                         <div className={styles.imageWrapper}>
                             <TimeImg />
@@ -108,31 +94,69 @@ export const CreateNewEntry = () => {
                         <span className={styles.placeholder}>
                             {!timeValue && <span>Время</span>}
                         </span>
-                        {timeIsOpen && (
-                            <div ref={timeRef} className={styles.timeRoot}>
-                                <div className={styles.timeTitle}>Выберете время</div>
-                                <div className={styles.timeWrapper}>
-                                    {timeArr.map((item, index) => (
-                                        <div
-                                            id={item === timeValue ? 'selected' : `${index}`}
-                                            key={item}
-                                            className={cls(styles.time, { [styles.activeTime]: item === timeValue })}
-                                            onClick={({ currentTarget }) => {
-                                                if (currentTarget?.textContent) {
-                                                    setTimeValue(currentTarget.textContent);
-                                                    setTimeIsOpen(!timeIsOpen);
-                                                }
-                                            }}
-                                        >
-                                            {item}
-                                        </div>)
-                                    )}
-                                </div>
-                            </div>
-                        )}
                     </div>
                 </div>
             </div>
+
+
+            {calendarPopup.isOpen && createPortal(
+                <div
+                    ref={calendarRef}
+                    style={{
+                        top: calendarPopup.top,
+                        left: calendarPopup.left
+                    }}
+                    className={styles.calendarWrapper}
+                >
+                    <DayPicker
+                        classNames={classNames}
+                        showOutsideDays
+                        defaultMonth={selectedDate}
+                        locale={ru}
+                        mode="single"
+                        selected={selectedDate}
+                        onDayClick={(date) => {
+                            if (date) {
+                                setSelectedDate(date);
+                                setDateValue(format(date, 'dd.MM.yyyy'));
+                                setCalendarPopup({ ...calendarPopup, isOpen: false });
+                            }
+                        }}
+                    />
+                </div>, document.body
+            )}
+
+
+
+            {timePopup.isOpen && createPortal(
+                <div
+                    ref={timeRef}
+                    style={{
+                        width: timePopup.width,
+                        top: timePopup.top,
+                        left: timePopup.left
+                    }}
+                    className={styles.timeRoot}>
+                    <div className={styles.timeTitle}>Выберете время</div>
+                    <div className={styles.timeWrapper}>
+                        {timeArr.map((item, index) => (
+                            <div
+                                id={item === timeValue ? 'selected' : `${index}`}
+                                key={item}
+                                className={cls(styles.time, { [styles.activeTime]: item === timeValue })}
+                                onClick={({ currentTarget }) => {
+                                    if (currentTarget?.textContent) {
+                                        setTimeValue(currentTarget.textContent);
+                                        setTimePopup({ ...timePopup, isOpen: false });
+                                    }
+                                }}
+                            >
+                                {item}
+                            </div>)
+                        )}
+                    </div>
+                </div>, document.body
+            )}
         </div>
     );
 };
