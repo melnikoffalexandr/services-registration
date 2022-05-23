55c9b30f3f61c21855d3a8a5c1e9c185da74f0a2
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-23 18:55:19 +0500

    test text color

--- src/pages/LayoutPage/index.tsx
+++ src/pages/LayoutPage/index.tsx
@@ -5,10 +5,10 @@ import { webAppTheme } from '../../utils/telegram';
 
 import styles from './layoutPage.module.scss';
 
-const { backgroundColor } = webAppTheme();
+const { backgroundColor, textColor } = webAppTheme();
 
 const LayoutPage = () => (
-    <div className={styles.root} style={{ background: backgroundColor }}>
+    <div className={styles.root} style={{ background: backgroundColor, color: textColor }}>
         <Outlet />
     </div>
 );
