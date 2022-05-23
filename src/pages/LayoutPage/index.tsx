b53ca0710977ac0059f43209be0ae35fbad3b90e
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-23 13:56:11 +0500

    test 11

--- src/pages/LayoutPage/index.tsx
+++ src/pages/LayoutPage/index.tsx
@@ -10,12 +10,17 @@ import styles from './layoutPage.module.scss';
 
 const LayoutPage = () => {
     const { height } = useWindowDimensions();
+
+    const percent = (innerHeight: number): number => {
+        console.log('test');
+        return (innerHeight / 100) * 60 - 48;
+    };
     return (
         <div className={styles.root}>
             <Outlet />
             <div
                 className={styles.buttonWrapper}
-                style={{ top: height - 48 }}
+                style={{ top: percent(height) }}
             >
                 <Button text="Временная кнопка, аналог главной кнопки" />
             </div>
