8d5010cb4df422b47794016f9590682b74364887
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-23 18:16:07 +0500

    test 31

--- src/components/MainButton/index.tsx
+++ src/components/MainButton/index.tsx
@@ -1,19 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
+import cls from 'classnames';
+
 import Button from '../Button';
+
+import { webAppIsExpanded } from '../../utils/telegram';
 
 import styles from './mainButton.module.scss';
 
 interface Props {
     isShow: boolean;
     text: string;
-    onClick: (event: any) => void;
+    onClick?: (event: any) => void;
 }
 
 const MainButton:FC<Props> = ({
     isShow = false, text = 'Временная кнопка', onClick,
 }) => {
+    const [height, setHeight] = useState<number>(window.Telegram.WebApp.viewportStableHeight - 61);
+    const isExpanded = webAppIsExpanded();
+
+    useEffect(() => {
+        if (!isExpanded) {
+            window.Telegram.WebApp.onEvent('viewportChanged', () => {
+                setHeight(window.Telegram.WebApp.viewportStableHeight - 61);
+            });
+        }
+    }, [height, isExpanded]);
+
     if (!isShow) {
         return null;
     }
@@ -21,7 +36,8 @@ const MainButton:FC<Props> = ({
     return (
         createPortal(
             <div
-                className={styles.root}
+                className={cls(styles.root, { [styles.rootExpanded]: isExpanded })}
+                style={{ top: !isExpanded ? height : undefined }}
                 onClick={onClick}
             >
                 <Button text={text} />
