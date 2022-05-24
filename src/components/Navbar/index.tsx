6cef20eab30ec47a8c507aa3866a2aeeba4ae9f3
Melnikov Alexandr <melnikoff.alexandr@gmail.com>
2022-05-24 10:38:24 +0500

    test animate 40

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../Button';
+import { useAppSelector } from '../../utils/hooks';
 
 import styles from './navbar.module.scss';
 
-const Navbar = () => (
-    <div className={styles.root}>
-        <div className={styles.title}>Записи</div>
-        <Link to="create-entry">
-            <Button className={styles.button} text="Записать" withIcon />
-        </Link>
-    </div>
-);
+const Navbar = () => {
+    const { userId } = useAppSelector((state) => state.app.user);
+    return (
+        <div className={styles.root}>
+            <div className={styles.title}>Записи</div>
+            <Link to={`create-entry/?${userId}`}>
+                <Button className={styles.button} text="Записать" withIcon />
+            </Link>
+        </div>
+    );
+};
 
 export default Navbar;
